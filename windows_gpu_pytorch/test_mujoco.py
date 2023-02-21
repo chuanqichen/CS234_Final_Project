0bbc1cc7406f751f2d2411b68923c28852c73a26
Chuanqi Chen <chuanqi.chen@gmail.com>
2023-02-20 23:45:15 -0600

    modified:   windows_gpu_pytorch/test_mujoco.py 	new file:   windows_gpu_pytorch/test_mujoco2.py 	new file:   windows_gpu_pytorch/test_robosuite_block_stacks.py

--- windows_gpu_pytorch/test_mujoco.py
+++ windows_gpu_pytorch/test_mujoco.py
@@ -2,7 +2,15 @@ import mujoco
 from mujoco import viewer
 import os
 
+<<<<<<< Updated upstream
 with open(os.path.join(os.path.join("model", "humanoid"), "humanoid.xml"), 'r') as f:
+=======
+<<<<<<< HEAD
+with open('model/humanoid/humanoid.xml', 'r') as f:
+=======
+with open(os.path.join(os.path.join("model", "humanoid"), "humanoid.xml"), 'r') as f:
+>>>>>>> 41e55e73f5ccaebc82286c87f6c7faac640cb801
+>>>>>>> Stashed changes
     XML = f.read()
 
 model = mujoco.MjModel.from_xml_string(XML)
