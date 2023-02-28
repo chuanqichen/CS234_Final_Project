d47fc3c54568302816db760a342a59155beda430
Kelvin Christian <kelvinchristian277@gmail.com>
2023-02-27 18:54:08 -0800

    add test.ipynb experiment

--- windows_gpu_pytorch/test_robosuite.py
+++ windows_gpu_pytorch/test_robosuite.py
@@ -3,8 +3,8 @@ import robosuite as suite
 
 # create environment instance
 env = suite.make(
-    env_name="Lift", # try with other tasks like "Stack" and "Door"
-    robots="Panda",  # try with other robots like "Sawyer" and "Jaco"
+    env_name="Stack", # try with other tasks like "Stack" and "Door"
+    robots="Sawyer",  # try with other robots like "Sawyer" and "Jaco"
     has_renderer=True,
     has_offscreen_renderer=False,
     use_camera_obs=False,
