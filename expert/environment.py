9b6bce63e0829dcf897bf7c6ec621ee078cfc013
edfouad <efouad@stanford.edu>
2023-03-13 22:20:19 -0400

    Custom use_object_obs parameter

--- expert/environment.py
+++ expert/environment.py
@@ -24,7 +24,7 @@ class Environment:
                 z_offset=0.01
         )
 
-    def create_env(self, fixed_placement=False):
+    def create_env(self, fixed_placement=False, use_object_obs=True):
         # create environment instance
         env = suite.make(
             env_name="Stack", # try with other tasks like "Stack" and "Door"
@@ -37,7 +37,7 @@ class Environment:
             control_freq=20,
             horizon=200,
             ignore_done=True,
-            use_object_obs=True,
+            use_object_obs=use_object_obs,
             use_camera_obs=True,
             camera_heights=64,
             camera_widths=64,
