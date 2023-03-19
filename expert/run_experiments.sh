a9f43759fec476710249c4a2a0e52ec3f6ad6133
Chuanqi Chen <chuanqi.chen@gmail.com>
2023-03-19 21:18:39 +0000

    modified:   expert/run_experiments.sh

--- expert/run_experiments.sh
+++ expert/run_experiments.sh
@@ -1,4 +1,9 @@
-CUDA_VISIBLE_DEVICES=0 python td3_with_obs.py --dirpath "test100" --filename "td3_fixed" --operation "both" --pi_arch 300,200,100 --qf_arch 300,200,100 --max_timesteps 10000000 --placement random --discount 0.98 &
-CUDA_VISIBLE_DEVICES=1 python td3_with_obs.py --dirpath "test200" --filename "td3_fixed" --operation "both" --pi_arch 300,100,50 --qf_arch 300,100,50 --max_timesteps 10000000 --placement random --discount 0.98 &
-CUDA_VISIBLE_DEVICES=2 python td3_with_obs.py --dirpath "test300" --filename "td3_fixed" --operation "both" --pi_arch 100,50,20 --qf_arch 100,50,20 --max_timesteps 10000000 --placement random --discount 0.98 &
-CUDA_VISIBLE_DEVICES=3 python td3_with_obs.py --dirpath "test400" --filename "td3_fixed" --operation "both" --pi_arch 50,20 --qf_arch 50,20 --max_timesteps 10000000 --placement random --discount 0.98 &
+CUDA_VISIBLE_DEVICES=0 python td3_with_obs.py --dirpath "test410" --filename "td3" --operation "both" --pi_arch 50,20 --qf_arch 50,20 --max_timesteps 10000000 --placement random --discount 0.98 &
+CUDA_VISIBLE_DEVICES=1 python td3_with_obs.py --dirpath "test411" --filename "td3" --operation "both" --pi_arch 50,20 --qf_arch 100,50,20 --max_timesteps 10000000 --placement random --discount 0.98 &
+CUDA_VISIBLE_DEVICES=2 python td3_with_obs.py --dirpath "test412" --filename "td3" --operation "both" --pi_arch 100,20 --qf_arch 100,20 --max_timesteps 10000000 --placement random --discount 0.98 &
+CUDA_VISIBLE_DEVICES=3 python td3_with_obs.py --dirpath "test413" --filename "td3" --operation "both" --pi_arch 100,20 --qf_arch 100,50,20 --max_timesteps 10000000 --placement random --discount 0.98 &
+
+CUDA_VISIBLE_DEVICES=0 python td3_with_obs.py --dirpath "test414" --filename "td3" --operation "both" --pi_arch 50,20 --qf_arch 50,20 --max_timesteps 10000000 --placement fixed --discount 0.98 &
+CUDA_VISIBLE_DEVICES=1 python td3_with_obs.py --dirpath "test415" --filename "td3" --operation "both" --pi_arch 50,20 --qf_arch 100,50,20 --max_timesteps 10000000 --placement fixed --discount 0.98 &
+CUDA_VISIBLE_DEVICES=2 python td3_with_obs.py --dirpath "test416" --filename "td3" --operation "both" --pi_arch 100,20 --qf_arch 100,20 --max_timesteps 10000000 --placement fixed --discount 0.98 &
+CUDA_VISIBLE_DEVICES=3 python td3_with_obs.py --dirpath "test417" --filename "td3" --operation "both" --pi_arch 100,20 --qf_arch 100,50,20 --max_timesteps 10000000 --placement fixed --discount 0.98 &
