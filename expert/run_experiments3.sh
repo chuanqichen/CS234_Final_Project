2a5fe7405f2a67733409c4d068a3c0d201dc6dc7
Kelvin Christian <kelvinchristian277@gmail.com>
2023-03-20 19:31:46 -0700

    add 3 experiments

---
+++ expert/run_experiments3.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=0 python ppo_with_obs.py --dirpath "small_obs_ppo_20_20_1e4_clip_04_d09" --filename "ppo" --operation "both" --pi 20,20 --vf 20,20 --max_timesteps 10000000 --placement random --discount 0.9 --learning_rate 0.001 --clip_range 0.4 &
+CUDA_VISIBLE_DEVICES=1 python ppo_with_obs.py --dirpath "small_obs_ppo_50_20_1e4_clip_04_d09" --filename "ppo" --operation "both" --pi 50,20 --vf 50,20 --max_timesteps 10000000 --placement random --discount 0.9 --learning_rate 0.001 --clip_range 0.4 &
+CUDA_VISIBLE_DEVICES=2 python td3_with_obs.py --dirpath "small_obs_td3_20_20_1e3_d09" --filename "td3" --operation "both" --pi 20,20 --vf 20,20 --max_timesteps 10000000 --placement random --discount 0.9 --learning_rate 0.001 &
+CUDA_VISIBLE_DEVICES=3 python td3_with_obs.py --dirpath "small_obs_td3_50_20_1e3_d09" --filename "td3" --operation "both" --pi 50,20 --vf 50,20 --max_timesteps 10000000 --placement random --discount 0.9 --learning_rate 0.001 &
