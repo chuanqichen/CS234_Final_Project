8bd26ea7cf530f318a30198870bbbf896e91bc6d
Kelvin Christian <kelvinchristian277@gmail.com>
2023-03-21 17:01:18 -0700

    ppo experiment

--- expert/run_ppo_experiment.sh
+++ expert/run_ppo_experiment.sh
@@ -1,4 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python ppo_with_obs.py --dirpath "ppo2_50_20_1e4" --filename "ppo" --operation "both" --pi 50,20 --vf 50,20 --max_timesteps 10000000 --placement random --discount 0.98 --learning_rate 0.0001 &
-CUDA_VISIBLE_DEVICES=1 python ppo_with_obs.py --dirpath "ppo2_50_20_1e4" --filename "ppo" --operation "both" --pi 100,50,20 --vf 100,50,20 --max_timesteps 10000000 --placement random --discount 0.98 --learning_rate 0.0001 &
-CUDA_VISIBLE_DEVICES=2 python ppo_with_obs.py --dirpath "ppo2_50_20_1e5" --filename "ppo" --operation "both" --pi 50,20 --vf 50,20 --max_timesteps 10000000 --placement random --discount 0.98 --learning_rate 0.00001 &
-CUDA_VISIBLE_DEVICES=3 python ppo_with_obs.py --dirpath "ppo2_50_20_1e5" --filename "ppo" --operation "both" --pi 100,50,20 --vf 100,50,20 --max_timesteps 10000000 --placement random --discount 0.98 --learning_rate 0.00001 &
+CUDA_VISIBLE_DEVICES=0 python ppo_with_obs.py --dirpath "small_obs_ppo_100_50_20_1e4_clip_07" --filename "ppo" --operation "both" --pi 100,50,20 --vf 100,50,20 --max_timesteps 10000000 --placement random --discount 0.98 --learning_rate 0.0001 --clip_range 0.4 &
